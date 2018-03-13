cd81940ffcc01d6a35d414f373816ab26f3b5a4d
Jaagup Kippar <jaagup@tlu.ee>
2018-03-13 11:11:48 +0200

    Update Testid1.java

--- src/test/java/kabe/Testid1.java
+++ src/test/java/kabe/Testid1.java
@@ -22,5 +22,12 @@ public class Testid1{
       laud.paiguta("A1", 'v');
       assertEquals('v', laud.kysi("A1"));
    }
-
+   @Test
+   public void alustamine(){
+      laud.algseis();
+      assertEquals('v', laud.kysi("A1"));
+      assertEquals('v', laud.kysi("B2"));
+      assertEquals('m', laud.kysi("A7"));
+      assertEquals('m', laud.kysi("B8"));
+   }
 }
