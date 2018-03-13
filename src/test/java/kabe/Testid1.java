715bad5c5f5b09d01e3331d96c5390627a9e7660
Jaagup Kippar <jaagup@tlu.ee>
2018-03-13 09:13:23 +0200

    Update Testid1.java

--- src/test/java/kabe/Testid1.java
+++ src/test/java/kabe/Testid1.java
@@ -2,6 +2,7 @@ package kabe;
 import org.junit.*;
 import static org.junit.Assert.*;
 public class Testid1{
+   KabeLaud laud=new JaagupiKabeLaud();
    @Test
    public void test1(){
       assertEquals(3, 1+2);
