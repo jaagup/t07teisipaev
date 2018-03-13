58fb1afd46f612efe4fd4b15305e4b2ca3ee4ad5
Jaagup Kippar <jaagup@tlu.ee>
2018-03-13 10:39:16 +0200

    Update KabeLaud.java

--- src/main/java/kabe/KabeLaud.java
+++ src/main/java/kabe/KabeLaud.java
@@ -2,4 +2,6 @@ package kabe;
 public interface KabeLaud{
    public int veergKoodiks(char veerg); //A->0, H->7
    public int ridaKoodiks(char rida); //8->0, 1->7
+   public void paiguta(String asukoht, char sisu);
+   public char kysi(String asukoht);
 }
